807f4d083092864f41ab517094adb60e64880d60
AlanBessa <alan.oliveira@btgpactual.com>
2017-09-15 14:37:00 -0300

    #76: Forms (template driven) Módulos e FormsModule

--- Angular2_Organized/Forms/src/app/app.module.ts
+++ Angular2_Organized/Forms/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TemplateFormModule } from './template-form/template-form.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -6,18 +7,17 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    TemplateFormModule,
     AppRoutingModule, 
     BsDropdownModule.forRoot()
   ],
