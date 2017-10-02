9eb505a33c34e57d8041e29275d87161555acdb0
AlanBessa <alan.oliveira@btgpactual.com>
2017-10-02 11:05:22 -0300

    Curso Angular #102: Módulo Shared: Organizando melhor o projeto

--- Angular2_Organized/Forms/src/app/shared/shared.module.ts
+++ Angular2_Organized/Forms/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { DropdownService } from './services/dropdown.service';
@@ -7,12 +8,19 @@ import { DropdownService } from './services/dropdown.service';
 import { FormDebugComponent } from './form-debug/form-debug.component';
 import { InputTextoComponent } from './input-texto/input-texto.component';
 
+
 @NgModule({
   imports: [
     CommonModule,
-    HttpModule
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
+    CommonModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
     FormDebugComponent,
     InputTextoComponent
   ],
--- Angular2_Organized/Forms/src/app/template-form/template-form.module.ts
+++ Angular2_Organized/Forms/src/app/template-form/template-form.module.ts
@@ -1,16 +1,10 @@
-import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from "@angular/forms";
 
 import { TemplateFormComponent } from "./template-form.component";
 import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    HttpModule,
     SharedModule
   ],
   declarations: [
